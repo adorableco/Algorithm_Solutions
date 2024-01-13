4d026e1a457ecf46c5c1e7ef8b9be7b7812e795e
adorableco <90397541+adorableco@users.noreply.github.com>
2024-01-13 20:07:06 +0900

    Time: 57 ms (80.13%), Space: 17.4 MB (22.26%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,27 +4,43 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
+    def toList(self,li: Optional[ListNode])-> []:
+        array = []
+        
+        while(li):
+            array.append(li.val)
+            li = li.next
+            
+        return array
+    
+    def reversedListNode(self,li: Optional[ListNode]) -> Optional[ListNode]:
+        node, prev = li, None
+        
+        while(node):
+            next, node.next = node.next, prev
+            prev, node = node, next
+        
+        return prev
+    
+    def toListNode(self,li: str)-> Optional[ListNode]:
+        root = node = ListNode(0)
+        
+        for i in li:
+            node.next = ListNode(i)
+            node = node.next
+            
+        return root.next
+            
+        
+        
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-            root = head = ListNode(0)
-            
-            
-            carry = 0
-            
-            while (l1 or l2 or carry):
-                sums = 0
-                if l1:
-                    sums += l1.val
-                    l1 = l1.next
-                if l2:
-                    sums += l2.val
-                    l2 = l2.next
-                    
-                carry, val = divmod(sums+carry, 10)
-                head.next = ListNode(val)
-                head = head.next
-            
-            
-            return root.next
+        str1 = int(''.join([str(e) for e in self.toList(self.reversedListNode(l1))]))
+        str2 = int(''.join([str(e) for e in self.toList(self.reversedListNode(l2))]))
+        
+        return self.reversedListNode(self.toListNode(str(str1+str2)))
+
+        
         
             
                 
