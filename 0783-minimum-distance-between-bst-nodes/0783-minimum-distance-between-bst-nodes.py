345d6804cbc5cb10109ed2897ac3671c98e52a2f
adorableco <90397541+adorableco@users.noreply.github.com>
2024-02-27 22:03:19 +0900

    Time: 34 ms (76.09%), Space: 16.5 MB (66.65%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -5,35 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    minimum = sys.maxsize
+    prev = -sys.maxsize
+    now = sys.maxsize
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
         
-        def dfs(target: int, node: TreeNode) -> int:
-            result = sys.maxsize
-            
-            if node.val <= target and node.right:
-                result = dfs(target, node.right)
+        if root:
+            self.minDiffInBST(root.left)
+            self.now = min(self.now, root.val - self.prev)
+            self.prev = root.val
+            self.minDiffInBST(root.right)
+        
+        return self.now
+             
 
-            elif node.val > target and node.left:
-                result = dfs(target, node.left)
-                    
-            else:
-                result = abs(node.val - target)
-            
-            return result
-        
-        minimum = sys.maxsize
-        queue = collections.deque([root])
-        while(queue):
-            now = queue.popleft()
-            print("queue = ",queue)
-            if now.left:
-                minimum = min(minimum, dfs(now.val, now.left))
-                queue.append(now.left)
-            if now.right:
-                minimum = min(minimum, dfs(now.val, now.right))
-                queue.append(now.right)
-        
-        return minimum
 
         
