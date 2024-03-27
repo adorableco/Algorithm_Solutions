f1aebbe9d09f9cf5edc01e941e70377314c4abbe
adorableco <90397541+adorableco@users.noreply.github.com>
2024-03-27 20:19:06 +0900

    Time: 111 ms (98.13%), Space: 32.5 MB (29.61%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,34 +5,21 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        array = []
         
-        if not (head and head.next):
-            return head
+        node = head
+        while(node):
+            array.append(node.val)
+            node = node.next
+        
+        node = head
+        
+        for i in sorted(array):
+            node.val = i
+            node = node.next
+        
+        return head
             
-        node, slow, fast = None, head, head
-        
-        while(fast and fast.next):
-            node, slow, fast = slow, slow.next, fast.next.next
-        
-        node.next = None
-        
-        l1 = self.sortList(head)
-        l2 = self.sortList(slow)
-        
-        return self.mergeTwoLists(l1,l2)
-    
-    def mergeTwoLists(self, l1: ListNode, l2:ListNode) -> Optional[ListNode]:
-        
-        if l1 and l2:
-            if l1.val > l2.val:
-                l1, l2 = l2, l1
-            
-            l1.next = self.mergeTwoLists(l1.next, l2)
-        
-        return l1 or l2
-                
-        
-        
         
         
         
