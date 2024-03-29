9e24d629609e41d1e147a49a0b6223beffad98ce
adorableco <90397541+adorableco@users.noreply.github.com>
2024-03-29 10:52:39 +0900

    Time: 114 ms (93.21%), Space: 20.5 MB (73.75%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,32 +2,17 @@ import collections
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = []
         
-        intervals = collections.deque(sorted(intervals))
-        result = []
-        
-        while(intervals):
+        for i in sorted(intervals, key = lambda x : x[0]):
+            if merged and i[0] <= merged[-1][1]:
+                merged[-1][1] = max(i[1], merged[-1][1])
             
-            if len(intervals) == 1:
-                result.append(intervals.popleft())
-                
-            
-            elif intervals[0][1] >= intervals[1][0]:
-                if intervals[0][1] < intervals[1][1]:
-                    new_list = [intervals[0][0], intervals[1][1]]
-                else: new_list = intervals[0]
-                    
-                intervals.append(new_list)
-                intervals.popleft()
-                intervals.popleft()
-                
             else:
-                result.append(intervals.popleft())   
-            
-            intervals = collections.deque(sorted(list(intervals)))
-        
-        return result
+                merged += i,
                 
+        return merged
+        
                 
             
         
