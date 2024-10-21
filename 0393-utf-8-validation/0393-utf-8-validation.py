3fc4a21dd596fb3f7d4e8848c0a881d3432dd791
Seyeon Park <90397541+adorableco@users.noreply.github.com>
2024-10-21 16:31:03 +0900

    Time: 2 ms (99.65%), Space: 17 MB (47.06%) - LeetHub

--- 0393-utf-8-validation/0393-utf-8-validation.py
+++ 0393-utf-8-validation/0393-utf-8-validation.py
@@ -1,27 +1,27 @@
 class Solution:
 
+
     def validUtf8(self, data: List[int]) -> bool:
 
         def check(count: int) -> bool:
-            for i in range(start + 1, start + count + 1):
-                if i >= len(data) or bin(data[i] >> 6) != "0b10":
+            for i in range(index + 1, index + count + 1):
+                if i >= len(data) or data[i] >> 6 != 0b10:
                     return False
-                
+            return True
+        
+        index = 0
 
-        start = 0
-    
-        while start < len(data):
-            if bin(data[start] >> 3) == "0b11110":
-                if check(3) == False: return False
-                start += 4
-            elif bin(data[start] >> 4) == "0b1110":
-                if check(2) == False: return False
-                start += 3
-            elif bin(data[start] >> 5) == "0b110":
-                if check(1) == False: return False
-                start += 2
-            elif bin(data[start] >> 7) == "0b0":
-                start += 1
+        while index < len(data):
+            start = data[index]
+
+            if start >> 3 == 0b11110 and check(3):
+                index += 4
+            elif start >> 4 == 0b1110 and check(2):
+                index += 3
+            elif start >> 5 == 0b110 and check(1):
+                index += 2
+            elif start >> 7 == 0b0:
+                index += 1
             else: return False
 
         return True
