0fcf37efa515e17d68b5722d080113ae42d6b02a
Seyeon Park <90397541+adorableco@users.noreply.github.com>
2024-12-20 13:14:39 +0900

    Time: 31 ms (10.04%), Space: 19.9 MB (5.41%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,17 +1,12 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        result = 0
-
-        g.sort(reverse = True)
+        g.sort()
         s.sort()
-        cookies = collections.deque(s)
 
-        for i in range(len(g)):
-            if not cookies:
-                break 
-
-            if cookies[len(cookies) - 1] >= g[i]:
-                cookies.pop()
+        result = 0
+        for i in s:
+            index = bisect.bisect_right(g, i)
+            if index > result:
                 result += 1
         
         return result
