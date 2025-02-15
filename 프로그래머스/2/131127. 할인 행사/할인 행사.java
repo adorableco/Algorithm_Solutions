85f07a6b52efb252bc3e544902ab7105752e043f
Seyeon Park <adorableco.sy@gmail.com>
2025-02-15 12:50:24 +0900

    [level 2] Title: 할인 행사, Time: 113.40 ms, Memory: 144 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        
+        HashMap<String, Integer> wantMap = new HashMap<>();
+        
+        for(int i = 0; i < want.length; i++){
+            wantMap.put(want[i], number[i]);
+        }
+        
+        for(int i = 0; i < discount.length - 9; i++){
+            HashMap<String, Integer> discountMap = new HashMap<>();
+            
+            for(int j = i; j < i + 10; j++){
+                if(wantMap.containsKey(discount[j]) && discountMap.getOrDefault(discount[j], 0) < wantMap.get(discount[j])){
+                    discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
+                }
+            }
+            if(wantMap.equals(discountMap)) answer += 1;
+        }
+        return answer;
+    }
+}
